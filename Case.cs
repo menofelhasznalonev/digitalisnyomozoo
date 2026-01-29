7a73b9927786b8036fda3309f159d711c2312add
Zsolt <beki.zsolt@petrik.hu>
2026-01-29 14:49:23 +0100

    Evidence hozzáadva

--- Case.cs
+++ Case.cs
@@ -13,15 +13,17 @@ namespace digitalisnyomozoo
 		private string leiras;
 		private string allapot;
 		private Person szemely;
-		//private Evidence bizonyitek;
+		private Evidence bizonyitek;
 
-		public Case(int id, string cim, string leiras, string allapot, Person szemely)
+
+		public Case(int id, string cim, string leiras, string allapot, Person szemely, Evidence bizonyitek)
 		{
 			this.id = id;
 			this.cim = cim;
 			this.leiras = leiras;
 			this.allapot = allapot;
 			this.szemely = szemely;
+			this.bizonyitek = bizonyitek;
 		}
 		
 
---
+++ Evidence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digitalisnyomozoo
+{
+	internal class Evidence
+	{
+		private int id;
+		private string tipus;
+		private string leiras;
+		private int megbizhatosagiErtek;
+
+		public Evidence(int id, string tipus, string leiras, int megbizhatosagiErtek)
+		{
+			this.id = id;
+			this.tipus = tipus;
+			this.leiras = leiras;
+			this.megbizhatosagiErtek = megbizhatosagiErtek;
+		}
+	}
+}
