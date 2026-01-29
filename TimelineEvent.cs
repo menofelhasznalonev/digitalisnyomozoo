4c5d208835ee9a668aa8dabed105ffcc0096384a
Zsolt <beki.zsolt@petrik.hu>
2026-01-29 14:51:02 +0100

    TimelineEvent hozzáadva

---
+++ TimelineEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digitalisnyomozoo
+{
+	internal class TimelineEvent
+	{
+		private DateTime datum;
+		private string esemenyLeirasa;
+
+		public TimelineEvent(DateTime datum, string esemenyLeirasa)
+		{
+			this.datum = datum;
+			this.esemenyLeirasa = esemenyLeirasa;
+		}
+	}
+}
