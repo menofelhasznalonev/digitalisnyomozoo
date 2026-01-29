0c5ebead21228c09c9a56800de65e12ac9c53abf
Zsolt <beki.zsolt@petrik.hu>
2026-01-29 14:46:19 +0100

    Witness hozzáadva

---
+++ Witness.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digitalisnyomozoo
+{
+	internal class Witness
+	{
+		private Person szemely;
+		private string vallomas;
+		private DateTime vallomasDatuma;
+
+		public Witness(Person szemely, string vallomas, DateTime vallomasDatuma)
+		{
+			this.szemely = szemely;
+			this.vallomas = vallomas;
+			this.vallomasDatuma = vallomasDatuma;
+		}
+	}
+}
