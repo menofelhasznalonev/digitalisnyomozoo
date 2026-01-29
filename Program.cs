39039bb040faed5f03353a97f1ef99bb9f1e0798
Zsolt <beki.zsolt@petrik.hu>
2026-01-29 13:52:05 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace digitalisnyomozoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
