b58ce97cc9323e09e17377f55f70f54e74c516d4
Zsolt <beki.zsolt@petrik.hu>
2026-01-29 14:31:51 +0100

    Case hozzáadva

---
+++ Case.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digitalisnyomozoo
+{
+	internal class Case
+	{
+		private int id;
+		private string cim;
+		private string leiras;
+		private string allapot;
+		//private Evidence bizonyitek;
+		//private Person szemely;
+		
+
+		public Case(int id, string cim, string leiras, string allapot)
+		{
+			this.id = id;
+			this.cim = cim;
+			this.leiras = leiras;
+			this.allapot = allapot;
+		}
+
+
+	}
+}
