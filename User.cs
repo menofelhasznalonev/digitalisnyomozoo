ab509880d36d8182dab6db671c81953c04d35e65
Zsolt <beki.zsolt@petrik.hu>
2026-01-29 14:22:35 +0100

    User.cs hozzáadva

---
+++ User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digitalisnyomozoo
+{
+	internal class User
+	{
+		private string nev;
+		private int id;
+		private string szerepkor;
+
+		public User(string nev, int id, string szerepkor)
+		{
+			this.nev = nev;
+			this.id = id;
+			this.szerepkor = szerepkor;
+		}
+	}
+}
