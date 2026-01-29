df3c8332d9807a3b2a1a87317798ec117461c0ef
Zsolt <beki.zsolt@petrik.hu>
2026-01-29 14:36:00 +0100

    CaseStatus hozzáadva

---
+++ CaseStatus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digitalisnyomozoo
+{
+	internal class CaseStatus
+	{
+		private string aktualisStatus;
+		private string feladata;
+		private string allapotvaltozasokKezelese;
+
+		public CaseStatus(string aktualisStatus, string feladata, string allapotvaltozasokKezelese)
+		{
+			this.aktualisStatus = aktualisStatus;
+			this.feladata = feladata;
+			this.allapotvaltozasokKezelese = allapotvaltozasokKezelese;
+		}
+	}
+}
